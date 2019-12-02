01e4269adfec1ab81098386c816a1dfb6fd0c25d
CallyB <carole.nissen@gmail.com>
2019-12-02 18:52:48 +0000

    update to trigger config vars

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
 import youtube from "./api/youtube";
 
