ae870e5596d168e113d27770779eba9cfa5d7cd7
CallyB <carole.nissen@gmail.com>
2021-06-06 12:45:32 +0100

    add moveRecord

--- functions/index.js
+++ functions/index.js
@@ -7,8 +7,10 @@ const sendConsentEmail = require('./sendConsentEmail.js')
 const sendWelcomeEmail = require('./sendWelcomeEmail.js')
 const sendgridNewVideo = require('./sendgridNewVideo')
 const sendWelcomeEmailAdult = require('./sendWelcomeEmailAdult.js')
+const moveRecord = require('./moveRecord.js')
 
 exports.sendConsentEmail = sendConsentEmail.sendConsentEmail;
 exports.sendWelcomeEmail = sendWelcomeEmail.sendWelcomeEmail;
 exports.sendgridNewVideo = sendgridNewVideo.sendgridNewVideo;
 exports.sendWelcomeEmailAdult = sendWelcomeEmailAdult.sendWelcomeEmailAdult;
+exports.moveRecord = moveRecord.moveRecord;
